7a850dd2fd2f3b9da3484f28f83a9309f3ae3fe5
Logan Williams <logan@firefly.llc>
2021-11-20 07:30:00 -0800

    Created store. Added guides data.

---
+++ src/lib/store.ts
@@ -0,0 +1,59 @@
+import { writable } from 'svelte/store';
+
+export const guides = writable([
+	{
+		body: [],
+		categories: [{ title: 'BG3', description: "Baldur's Gate 3" }],
+		image: {
+			url: '/images/guides/bg3-class-tier-list.webp',
+			alt: "Baldur's Gate 3 standard party characters standing in a diamond formation."
+		},
+		publishedAt: '2021-03-21T15:00:00.000Z',
+		slug: 'baldur-s-gate-3-class-tier-list-and-guide',
+		title: "Baldur's Gate 3 Class Tier List and Guide",
+		excerpt:
+			'This is entry three of four in my Baldur’s Gate 3 rankings and guide series where I review the 7 classes and the 15 subclasses available in the early access so far.',
+		updatedAt: '2021-11-19T21:11:28Z'
+	},
+	{
+		body: [],
+		categories: [{ title: 'BG3', description: "Baldur's Gate 3" }],
+		image: {
+			url: '/images/guides/bg3-ability-strength.webp',
+			alt: "Gauntlet signifying strength in Baldur's Gate 3"
+		},
+		publishedAt: '2021-02-17T09:00:00.000Z',
+		slug: 'baldurs-gate-3-ability-guide',
+		title: "Baldur's Gate 3 - Ability Guide",
+		excerpt:
+			'This is the first article in a four part series where I analyze and rank fundamental build choices in Baldur’s Gate 3 (BG3). I will be ranking abilities, races, classes, and spells. In this article I analyze and rank abilities. Not all abilities are equal!',
+		updatedAt: '2021-03-03T05:45:48Z'
+	},
+	{
+		body: [],
+		categories: [{ title: 'BG1', description: "Baldur's Gate 1" }],
+		image: {
+			url: '/images/guides/flyingSolo.webp',
+			alt: 'Steam flying solo achievement with thumbs up icon'
+		},
+		publishedAt: '2021-03-29T18:41:12.176Z',
+		slug: 'three-must-play-solo-builds-for-baldurs-gate-1',
+		title: 'Three Must-Play Solo Builds for Baldur’s Gate I',
+		excerpt: 'My recommendations for extremely fun solo builds for BGI',
+		updatedAt: '2021-03-30T15:33:07Z'
+	},
+	{
+		body: [],
+		categories: [{ title: 'BG3', description: "Baldur's Gate 3" }],
+		image: {
+			url: '/images/guides/bg3-race-tier-list.webp',
+			alt: "Baldur's Gate 3 High Elf portrait"
+		},
+		publishedAt: '2021-02-21T08:00:00.000Z',
+		slug: 'baldurs-gate-3-race-tier-list',
+		title: "Baldur's Gate 3 - Race Tier List",
+		excerpt:
+			'I rank the races available in BG3 early access. I cannot stress enough that BG3 is a video game. If you find this because you have googled something you will hopefully regret under the influence of rage, fear, and ethnic insecurity, I am not on your side. Find someone qualified to help you.',
+		updatedAt: '2021-11-04T23:06:39Z'
+	}
+]);
