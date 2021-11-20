1ca7ebd823292220993be9088be4763b0f0ae373
Logan Williams <logan@firefly.llc>
2021-11-20 07:32:35 -0800

    Configured tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,35 @@ const config = {
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ['Petrona', 'Helvetica', 'Arial', 'sans-serif']
+		},
+
+		extend: {
+			colors: {
+				parchment: '#faf3d1',
+				bgBrown: '#261210',
+				bgRed: '#591812',
+				bgYellow: '#D8C368',
+				bgGrey: '#737373',
+				bgBlack: '#0D0D0D'
+			},
+			maxWidth: {
+				'8xl': '90rem'
+			}
+		}
 	},
 
-	plugins: []
+	variants: {
+		display: ['group-hover']
+	},
+
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography')
+	]
 };
 
 module.exports = config;
