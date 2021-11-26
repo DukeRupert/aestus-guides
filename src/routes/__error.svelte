4d2fdd74d80626e04f40da4f5be592573a7bab1d
Logan Williams <logan@firefly.llc>
2021-11-26 07:53:06 -0800

    Created 404 error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,102 @@
+<div class="bg-parchament dark:bg-black">
+	<main class="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+		<div class="max-w-xl mx-auto py-16 sm:py-24">
+			<div class="text-center">
+				<p class="text-sm font-semibold text-indigo-600 uppercase tracking-wide">404 error</p>
+				<h1
+					class="mt-2 text-4xl font-extrabold text-gray-900 dark:text-gray-300 tracking-tight sm:text-5xl"
+				>
+					This page does not exist.
+				</h1>
+				<p class="mt-2 text-lg text-gray-500 dark:text-gray-400">
+					The page you are looking for could not be found.
+				</p>
+			</div>
+			<div class="mt-12">
+				<h2 class="text-sm font-semibold text-gray-500 dark:text-gray-400 tracking-wide uppercase">
+					Popular pages
+				</h2>
+				<ul role="list" class="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+					<li class="relative py-6 flex items-start space-x-4">
+						<div class="flex-shrink-0">
+							<span class="flex items-center justify-center h-12 w-12 rounded-lg">
+								<img class="h-12 w-12" src="/images/bg3.webp" alt="Baldurs Gate 3 logo" />
+							</span>
+						</div>
+						<div class="min-w-0 flex-1">
+							<h3 class="text-base font-medium text-gray-900 dark:text-gray-300">
+								<span
+									class="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+								>
+									<a href="/guides/baldurs-gate-3" class="focus:outline-none">
+										<span class="absolute inset-0" aria-hidden="true" />
+										Baldur's Gate 3 Guides
+									</a>
+								</span>
+							</h3>
+							<p class="text-base text-gray-500">Helpful guides for Baldur's Gate 3</p>
+						</div>
+						<div class="flex-shrink-0 self-center">
+							<!-- Heroicon name: solid/chevron-right -->
+							<svg
+								class="h-5 w-5 text-gray-400"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+					</li>
+
+					<li class="relative py-6 flex items-start space-x-4">
+						<div class="flex-shrink-0">
+							<span class="flex items-center justify-center h-12 w-12 rounded-lg">
+								<img class="h-12 w-12" src="/images/bg1.webp" alt="Baldurs Gate 1 logo" />
+							</span>
+						</div>
+						<div class="min-w-0 flex-1">
+							<h3 class="text-base font-medium text-gray-900 dark:text-gray-300">
+								<span
+									class="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+								>
+									<a href="/guides/baldurs-gate-1" class="focus:outline-none">
+										<span class="absolute inset-0" aria-hidden="true" />
+										Baldur's Gate 1 Guides
+									</a>
+								</span>
+							</h3>
+							<p class="text-base text-gray-500">Helpful guides for Baldur's Gate 1</p>
+						</div>
+						<div class="flex-shrink-0 self-center">
+							<!-- Heroicon name: solid/chevron-right -->
+							<svg
+								class="h-5 w-5 text-gray-400"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+					</li>
+				</ul>
+				<div class="mt-8">
+					<a href="/" class="text-base font-medium text-indigo-600 hover:text-indigo-500"
+						>Or go back home<span aria-hidden="true"> &rarr;</span></a
+					>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
