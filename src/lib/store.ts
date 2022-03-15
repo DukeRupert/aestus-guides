95cc91dc06066c3826e743b8b009ab060cc5635c
Logan Williams <logan@firefly.llc>
2022-03-15 12:32:29 -0700

    Restored guides, bg3Guides and bg1Guides objects to store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,6 +2,153 @@ import { writable } from 'svelte/store';
 
 export const dark = writable(false);
 
+export const guides = writable([
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-spell-tier-list-main.webp',
+			alt: "Baldur's Gate 3 standard party characters standing in a diamond formation."
+		},
+		publishedAt: '2021-12-24T15:00:00.000Z',
+		slug: 'baldurs-gate-3-spell-tier-list',
+		title: "Baldur's Gate 3 Spell Tier List",
+		excerpt:
+			'This is the fourth and final article in my Baldur’s Gate 3 rankings and guide series where I review the spells available in the early access so far.',
+		updatedAt: '2021-12-24T21:11:28Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-class-tier-list.webp',
+			alt: "Baldur's Gate 3 standard party characters standing in a diamond formation."
+		},
+		publishedAt: '2021-03-21T15:00:00.000Z',
+		slug: 'baldur-s-gate-3-class-tier-list-and-guide',
+		title: "Baldur's Gate 3 Class Tier List and Guide",
+		excerpt:
+			'This is entry three of four in my Baldur’s Gate 3 rankings and guide series where I review the 7 classes and the 15 subclasses available in the early access so far.',
+		updatedAt: '2021-11-19T21:11:28Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-ability-strength.webp',
+			alt: "Gauntlet signifying strength in Baldur's Gate 3"
+		},
+		publishedAt: '2021-02-17T09:00:00.000Z',
+		slug: 'baldurs-gate-3-ability-guide',
+		title: "Baldur's Gate 3 - Ability Guide",
+		excerpt:
+			'This is the first article in a four part series where I analyze and rank fundamental build choices in Baldur’s Gate 3 (BG3). I will be ranking abilities, races, classes, and spells. In this article I analyze and rank abilities. Not all abilities are equal!',
+		updatedAt: '2021-03-03T05:45:48Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG1', description: "Baldur's Gate 1", link: 'baldurs-gate-1' },
+		image: {
+			url: '/images/guides/flyingSolo.webp',
+			alt: 'Steam flying solo achievement with thumbs up icon'
+		},
+		publishedAt: '2021-03-29T18:41:12.176Z',
+		slug: 'three-must-play-solo-builds-for-baldurs-gate-1',
+		title: 'Three Must-Play Solo Builds for Baldur’s Gate I',
+		excerpt: 'My recommendations for extremely fun solo builds for BGI',
+		updatedAt: '2021-03-30T15:33:07Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-race-tier-list.webp',
+			alt: "Baldur's Gate 3 High Elf portrait"
+		},
+		publishedAt: '2021-02-21T08:00:00.000Z',
+		slug: 'baldurs-gate-3-race-tier-list',
+		title: "Baldur's Gate 3 - Race Tier List",
+		excerpt:
+			'I rank the races available in BG3 early access. I cannot stress enough that BG3 is a video game. If you find this because you have googled something you will hopefully regret under the influence of rage, fear, and ethnic insecurity, I am not on your side. Find someone qualified to help you.',
+		updatedAt: '2021-11-04T23:06:39Z'
+	}
+]);
+
+export const bg3Guides = writable([
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-spell-tier-list-main.webp',
+			alt: "Baldur's Gate 3 standard party characters standing in a diamond formation."
+		},
+		publishedAt: '2021-12-24T15:00:00.000Z',
+		slug: 'baldurs-gate-3-spell-tier-list',
+		title: "Baldur's Gate 3 Spell Tier List",
+		excerpt:
+			'This is the fourth and final article in my Baldur’s Gate 3 rankings and guide series where I review the spells available in the early access so far.',
+		updatedAt: '2021-12-24T21:11:28Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-class-tier-list.webp',
+			alt: "Baldur's Gate 3 standard party characters standing in a diamond formation."
+		},
+		publishedAt: '2021-03-21T15:00:00.000Z',
+		slug: 'baldur-s-gate-3-class-tier-list-and-guide',
+		title: "Baldur's Gate 3 Class Tier List and Guide",
+		excerpt:
+			'This is entry three of four in my Baldur’s Gate 3 rankings and guide series where I review the 7 classes and the 15 subclasses available in the early access so far.',
+		updatedAt: '2021-11-19T21:11:28Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-ability-strength.webp',
+			alt: "Gauntlet signifying strength in Baldur's Gate 3"
+		},
+		publishedAt: '2021-02-17T09:00:00.000Z',
+		slug: 'baldurs-gate-3-ability-guide',
+		title: "Baldur's Gate 3 - Ability Guide",
+		excerpt:
+			'This is the first article in a four part series where I analyze and rank fundamental build choices in Baldur’s Gate 3 (BG3). I will be ranking abilities, races, classes, and spells. In this article I analyze and rank abilities. Not all abilities are equal!',
+		updatedAt: '2021-03-03T05:45:48Z'
+	},
+	{
+		body: [],
+		game: { title: 'BG3', description: "Baldur's Gate 3", link: 'baldurs-gate-3' },
+		image: {
+			url: '/images/guides/bg3-race-tier-list.webp',
+			alt: "Baldur's Gate 3 High Elf portrait"
+		},
+		publishedAt: '2021-02-21T08:00:00.000Z',
+		slug: 'baldurs-gate-3-race-tier-list',
+		title: "Baldur's Gate 3 - Race Tier List",
+		excerpt:
+			'I rank the races available in BG3 early access. I cannot stress enough that BG3 is a video game. If you find this because you have googled something you will hopefully regret under the influence of rage, fear, and ethnic insecurity, I am not on your side. Find someone qualified to help you.',
+		updatedAt: '2021-11-04T23:06:39Z'
+	}
+]);
+
+export const bg1Guides = writable([
+	{
+		body: [],
+		game: { title: 'BG1', description: "Baldur's Gate 1", link: 'baldurs-gate-1' },
+		image: {
+			url: '/images/guides/flyingSolo.webp',
+			alt: 'Steam flying solo achievement with thumbs up icon'
+		},
+		publishedAt: '2021-03-29T18:41:12.176Z',
+		slug: 'three-must-play-solo-builds-for-baldurs-gate-1',
+		title: 'Three Must-Play Solo Builds for Baldur’s Gate I',
+		excerpt: 'My recommendations for extremely fun solo builds for BGI',
+		updatedAt: '2021-03-30T15:33:07Z'
+	}
+]);
+
 export const siteSettings = writable({
 	_createdAt: '2019-03-29T10:09:19Z',
 	_id: 'siteSettings',
